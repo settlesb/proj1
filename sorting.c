dc3eb4fd2b0abab7d1567f59d75ea623eb1ec2e9
Bryan Settles <Bryansettles19@gmail.com>
2017-09-08 15:52:44 -0400

    first commit

---
+++ sorting.c
@@ -0,0 +1,64 @@
+#include  <stdio.h>
+void printArray(int arr[], int n);
+void bubbleSort(int arr[], int n);
+void swap(long *xp, long *yp);
+void Shell_Insertion_Sort (long *Array, int Size, double *NComp, double *NMove);
+void Improved_Bubble_Sort (long *Array, int Size, double *NComp, double *NMove);
+long *Load_File (char *Filename, int *Size);
+int Save_File (char *Filename, long *Array, int Size);
+void Save_Seq1 (char *Filename, int N);
+void Save_Seq2 (char *Filename, int N);
+
+void Shell_Insertion_Sort(long *Array, int Size, double *NComp, double *NMove)
+{
+
+    for (int gap = Size/2; gap > 0; gap /= 2)
+    {
+
+        for (int i = gap; i < Size; i += 1)
+        {
+            long temp = Array[i];
+
+            int j;            
+            for (j = i; j >= gap && Array[j - gap] > temp; j -= gap)
+            {
+                Array[j] = Array[j - gap];
+            }
+             
+            Array[j] = temp;
+        }
+    }
+    return;
+}
+void Improved_Bubble_Sort (long *Array, int Size, double *NComp, double *NMove)
+{
+   int i, j;
+   for (i = 0; i < Size-1; i++)      
+  {
+       for (j = 0; j < Size-i-1; j++) 
+       {
+           if (Array[j] > Array[j+1])
+           {
+              swap(&Array[j], &Array[j+1]);
+              NMove++;
+           }
+       }
+  }
+}
+void swap(long *xp, long *yp)
+{
+    long temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+ 
+void printArray(long arr[], int n)
+{
+    for (int i=0; i<n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+ 
