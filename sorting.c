299bcc4a2958d8fbfac869a46c874066783aa1ac
Bryan Settles <Bryansettles19@gmail.com>
2017-09-09 19:37:02 -0400

    update

--- sorting.c
+++ sorting.c
@@ -1,75 +1,131 @@
 #include  <stdio.h>
 #include "sorting.h"
-void printArray(long arr[], int n);
+#include <stdbool.h>
+#include <math.h>
 void swap(long *xp, long *yp);
 void Save_Seq1 (char *Filename, int N);
 void Save_Seq2 (char *Filename, int N);
+int getNextGap(int gap);
+int cmpfunc(const void * a, const void * b);
+
+//handle bad file input??
+//turnin within ecegrid account, or specifically in git cloned repo??
+//gap sequence??
+//save function/sequence
+
+void Save_Seq1 (char *Filename, int N)
+{
+  FILE * pFile;
+  pFile = fopen (Filename,"w");
+  fprintf(pFile, "");
+  fclose(pFile);
+}
+
+void Save_Seq2 (char *Filename, int N)
+{
+  FILE * pFile;
+  pFile = fopen (Filename,"w");
+  fprintf(pFile, "");
+  fclose(pFile);
+}
 
 long *Load_File(char *Filename, int *Size)
 {
-    FILE * pFile;
-    pFile = fopen (Filename,"r");
-    fscanf(pFile, "%d\n", Size);
-    int i = 0;
-    long arr[*Size];
-    while(fscanf(pFile, "%ld\n", &arr[i]) == 1)
-    {
-      i++;
-    }
-    fclose(pFile);
-    return arr;
+  FILE * pFile;
+  pFile = fopen (Filename,"r");
+  fscanf(pFile, "%d\n", Size);
+  int i = 0;
+  long * arr = malloc(sizeof(long) * *Size);
+  while(fscanf(pFile, "%ld\n", &arr[i]) == 1)
+  {
+    i++;
+  }
+  fclose(pFile);
+  return arr;
 }
 int Save_File(char *Filename, long *Array, int Size)
 {
-    int i;
-    FILE * pFile;
-    pFile = fopen (Filename,"w");
-    for(i = 0;i<Size;i++)
-    {
-      fprintf(pFile, "%ld \n", Array[i]);
-    }
-    fclose(pFile);
-    return i+1;
+  int i;
+  FILE * pFile;
+  pFile = fopen (Filename,"w");
+  fprintf(pFile, "%d\n", Size);
+  for(i = 0;i<Size;i++)
+  {
+    fprintf(pFile, "%ld\n", Array[i]);
+  }
+  fclose(pFile);
+  return i;
 }
-
+int cmpfunc(const void * a, const void * b)
+{
+   return ( *(int*)a - *(int*)b );
+}
 void Shell_Insertion_Sort(long *Array, int Size, double *NComp, double *NMove)
 {
-    int gap, i;
-    for (gap = Size/2; gap > 0; gap /= 2)
+  int *arr = malloc(sizeof(int) * Size);
+  int ix= 0;
+  int pow2, pow3 = 1;
+  while(pow3 <= Size)
+  {
+      pow2 = pow3;
+      while(pow2 <= Size)
+      {
+        arr[ix] = pow2;
+        pow2 = pow2 * 2;
+        ix++;
+      }
+      pow3 = pow3 * 3;
+  }
+  arr[ix] = '\0';
+  qsort(arr, ix, sizeof(int), cmpfunc);
+
+  int gap, i;
+  for (gap = arr[ix-1]; gap > 0; gap = arr[ix-1])
+  {
+      for (i = gap; i < Size; i += 1)
+      {
+          long temp = Array[i];
+
+          int j;            
+          for (j = i; j >= gap && Array[j - gap] > temp; j -= gap)
+          {
+              Array[j] = Array[j - gap];
+              *NMove+=1;
+              *NComp+=1;
+          }
+          Array[j] = temp;
+      }
+      ix--;
+  }
+  free(arr);
+  return;
+}
+int getNextGap(int gap)
+{
+    gap = (gap*10)/13;
+    if (gap < 1)
+        return 1;
+    return gap;
+}
+void Improved_Bubble_Sort(long *Array, int Size, double *NComp, double *NMove)
+{
+    int gap = Size;
+    bool swapped = true;
+    while (gap != 1 || swapped == true)
     {
-
-        for (i = gap; i < Size; i += 1)
+        gap = getNextGap(gap);
+        swapped = false;
+        for (int i=0; i<Size-gap; i++)
         {
-            long temp = Array[i];
-
-            int j;            
-            for (j = i; j >= gap && Array[j - gap] > temp; j -= gap)
+            *NComp+=1;
+            if (Array[i] > Array[i+gap])
             {
-                Array[j] = Array[j - gap];
-                NMove++;
-                NComp++;
+                swap(&Array[i], &Array[i+gap]);
+                *NMove+=1;
+                swapped = true;
             }
-             
-            Array[j] = temp;
         }
     }
-    return;
-}
-void Improved_Bubble_Sort (long *Array, int Size, double *NComp, double *NMove)
-{
-   int i, j;
-   for (i = 0; i < Size-1; i++)      
-  {
-       for (j = 0; j < Size-i-1; j++) 
-       {
-           if (Array[j] > Array[j+1])
-           {
-              swap(&Array[j], &Array[j+1]);
-              NMove++;
-           }
-           NComp++;
-       }
-  }
 }
 void swap(long *xp, long *yp)
 {
@@ -78,14 +134,3 @@ void swap(long *xp, long *yp)
     *yp = temp;
 }
 
- 
-void printArray(long arr[], int n)
-{
-    int i;
-    for (i=0; i<n; i++)
-    {
-        printf("%ld ", arr[i]);
-    }
-    printf("\n");
-}
- 
