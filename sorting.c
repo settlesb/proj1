347b29cdbe124bfff008b60d868529e2f9ff1a03
Bryan Settles <Bryansettles19@gmail.com>
2017-09-10 09:33:23 -0400

    update

--- sorting.c
+++ sorting.c
@@ -8,16 +8,32 @@ void Save_Seq2 (char *Filename, int N);
 int getNextGap(int gap);
 int cmpfunc(const void * a, const void * b);
 
-//handle bad file input??
-//turnin within ecegrid account, or specifically in git cloned repo??
-//gap sequence??
-//save function/sequence
+//save_seq need to be called by anything
 
 void Save_Seq1 (char *Filename, int N)
 {
   FILE * pFile;
   pFile = fopen (Filename,"w");
-  fprintf(pFile, "");
+  int *arr = malloc(sizeof(int) * N);
+  int ix= 0;
+  int pow2, pow3 = 1;
+  while(pow3 <= N)
+  {
+      pow2 = pow3;
+      while(pow2 <= N)
+      {
+        arr[ix] = pow2;
+        pow2 = pow2 * 2;
+        ix++;
+      }
+      pow3 = pow3 * 3;
+  }
+  arr[ix] = '\0';
+  qsort(arr, ix, sizeof(int), cmpfunc);
+  for(ix=0;arr[ix]!='\0';ix++)
+  {
+    fprintf(pFile, "%d\n", arr[ix]);
+  }
   fclose(pFile);
 }
 
@@ -25,7 +41,20 @@ void Save_Seq2 (char *Filename, int N)
 {
   FILE * pFile;
   pFile = fopen (Filename,"w");
-  fprintf(pFile, "");
+  int arr[N/40], i = 0;
+  int gap = N;
+  while (gap != 1)
+  {
+      gap = getNextGap(gap);
+      arr[i] = gap;
+      i++;
+  }
+  arr[i] = '\0';
+  qsort(arr, i, sizeof(int), cmpfunc);
+  for(i = 0;arr[i] != '\0';i++)
+  {
+    fprintf(pFile, "%d\n", arr[i]);
+  }
   fclose(pFile);
 }
 
@@ -78,15 +107,14 @@ void Shell_Insertion_Sort(long *Array, int Size, double *NComp, double *NMove)
   }
   arr[ix] = '\0';
   qsort(arr, ix, sizeof(int), cmpfunc);
-
   int gap, i;
   for (gap = arr[ix-1]; gap > 0; gap = arr[ix-1])
   {
       for (i = gap; i < Size; i += 1)
       {
-          long temp = Array[i];
-
-          int j;            
+          long temp = Array[i]; 
+          *NMove+=1;
+          int j;         
           for (j = i; j >= gap && Array[j - gap] > temp; j -= gap)
           {
               Array[j] = Array[j - gap];
@@ -94,6 +122,7 @@ void Shell_Insertion_Sort(long *Array, int Size, double *NComp, double *NMove)
               *NComp+=1;
           }
           Array[j] = temp;
+          *NMove+=1;
       }
       ix--;
   }
@@ -105,6 +134,10 @@ int getNextGap(int gap)
     gap = (gap*10)/13;
     if (gap < 1)
         return 1;
+    else if((gap == 10) || (gap == 9))
+    {
+      return 11;
+    }
     return gap;
 }
 void Improved_Bubble_Sort(long *Array, int Size, double *NComp, double *NMove)
